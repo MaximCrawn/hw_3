350fdd0f2683f74bb9a98ede974547b308d10239
Maxim <Crawn@crawn.ru>
2024-03-16 09:41:54 +0300

    Изменил Main

--- Main.java
+++ Main.java
@@ -2,12 +2,19 @@ package hw_3;
 public class Main {
     public static void main(String[] args) {
     MyLinkedList list = new MyLinkedList();
-    list.addFirst("1");
-    list.addFirst("2");
-    list.addFirst("3");
-    list.addFirst("4");
+    list.addLast("1");
+    list.addLast("2");
+    list.addLast("3");
+    list.addLast("4");
     list.addFirst("5");
 
+    // Использование цикла for-each для перебора элементов списка
+    System.out.println("Элементы до удаления");
+    for (String element : list) {
+        System.out.println(element);
+    }
+    list.removeFirst();
+    System.out.println("Элементы после удаления");
     // Использование цикла for-each для перебора элементов списка
     for (String element : list) {
         System.out.println(element);
